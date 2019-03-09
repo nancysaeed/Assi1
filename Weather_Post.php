54a5dad8ba118327444b7d36bd91dacae548df46
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-03-09 22:27:01 +0200

    Add files via upload

---
+++ Weather_Post.php
@@ -0,0 +1,37 @@
+<?php
+$city=$_POST["city"];
+$curl = curl_init();
+  //print($city);
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://samples.openweathermap.org/data/2.5/weather?q=".$city.",uk&appid=19d6178f3b8425f989e37a57e86a0879",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "",
+  CURLOPT_HTTPHEADER => array(
+    "Postman-Token: 93bbe2cd-9840-4645-9fcd-d471e6d2c0e9",
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  //echo $response;
+  $x =json_decode($response);
+  echo "<br />";
+  print("Weather For ");
+  print($city);
+  echo "<br />";
+	
+}
+?>
